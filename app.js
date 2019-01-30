cc91b802c9a393698347c249f4de71eb16ee9028
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-01-30 19:04:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true }));
-var campGrounds = [
-       {name:"salmon hill",image:"https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f8c770a0e4bdbb_340.jpg"},
-       {name:"mount abu", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104491f8c770a0e4bdbb_340.jpg"}
-]; 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
-app.set("view engine","ejs");
-
-app.get("/",function(req,res){
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+    
+app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/campgrounds",function(req,res){
-  
-       res.render("campgrounds",{campGrounds:campGrounds});
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds",{campgrounds:campgrounds});
 });
 
-
-app.post("/campgrounds",function(req,res){
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {
-       name:name,image:image
-   }
-   campGrounds.push(newCampground);
-   
-   res.redirect("/campgrounds");
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
 });
 
-app.get("/campgrounds/new",function(req,res){
-  res.render("new");  
+app.get("/campgrounds/new", function(req, res){
+   res.render("new.ejs"); 
 });
 
-app.listen(process.env.PORT,process.env.IP,function(){
-   console.log("The yelp camp server has  started!"); 
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
