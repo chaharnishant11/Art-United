121954f7c5abc1c0eb10db6a71831b27dbee475d
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-02-03 14:57:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,89 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
-mongoose.connect("mongodb://localhost/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-   name:  String,
-   image: String
+   name: String,
+   image: String,
+   description: String
 });
 
-var Campground = mongoose.model("Campground",campgroundSchema);
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-/*Campground.create(
-    {
-            name: "Salmon Creek", 
-            image:"https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
-    },function(err,campground){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(campground);
-        }
-        
-});*/
+// Campground.create(
+//      {
+//          name: "Granite Hill", 
+//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+         
+//      },
+//      function(err, campground){
+//       if(err){
+//           console.log(err);
+//       } else {
+//           console.log("NEWLY CREATED CAMPGROUND: ");
+//           console.log(campground);
+//       }
+//     });
+
     
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-    Campground.find({},function(err,allCampgrounds){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("campgrounds",{campgrounds:allCampgrounds}); 
-        }
-    })
-    
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("index",{campgrounds:allCampgrounds});
+       }
+    });
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    Campground.create(newCampground,function(err,newlyCreated){
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
-        }else{
+        } else {
             //redirect back to campgrounds page
-            res.redirect("/campgrounds");   
+            res.redirect("/campgrounds");
         }
     });
-    
-   
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
 });
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
