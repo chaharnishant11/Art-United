4fdd6c639da7744a997cdd12f5e44710ef21a82f
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-02-12 14:56:12 +0530

    Modifying the file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
+mongoose.connect("mongodb+srv://user:<ABC!!d12>@cluster0-kbyuw.mongodb.net/yelp_camp?retryWrites=true",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
